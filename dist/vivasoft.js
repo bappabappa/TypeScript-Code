53e6370bbe4f918f19b3ce434fe61c239129eb8f
bappasaha <45108418+bappabappa@users.noreply.github.com>
2022-07-20 13:02:40 +0600

    programming hero practise code

---
+++ dist/vivasoft.js
@@ -0,0 +1,7 @@
+"use strict";
+// console.log("fucntioin");
+function greeting(message) {
+    console.log(message);
+}
+const sms = greeting("hello baoo bar");
+// console.log(sms)
---
+++ src/vivasoft.ts
@@ -0,0 +1,8 @@
+// console.log("fucntioin");
+
+function greeting(message:string):void{
+    console.log(message);
+}
+
+const sms =greeting("hello baoo bar");
+// console.log(sms)
